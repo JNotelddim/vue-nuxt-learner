e3c527e7e99179bd4df0d1c8bc6e66aad570d1ed
Jared McKimm <jaredmiddleton3.14@gmail.com>
2024-07-25 14:58:08 -0700

    Init Nuxt app

---
+++ components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Welcome to My App!</h1>
+    <p>Thank you for visiting.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #333;
+  font-size: 24px;
+}
+
+p {
+  color: #666;
+  font-size: 16px;
+}
+</style>
